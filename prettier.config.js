c2ab1035d60511fc4a4858decf3080dc0001f416
lumen-limitless <lumenlimitless@gmail.com>
2023-12-10 10:38:14 -0500

    Update prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: false,
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-packagejson'],
+}
