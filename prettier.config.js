71b0183e55c94411a83c94112104c35a6f812d32
lumen-limitless <lumenlimitless@gmail.com>
2024-01-21 13:49:38 -0500

    Update dependencies and fix formatting issues

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,5 @@ module.exports = {
   tabWidth: 2,
   useTabs: false,
   singleQuote: true,
-  semi: false,
   plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-packagejson'],
-}
+};
--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,7 +1,7 @@
-import { describe, expect, it } from 'bun:test'
+import { describe, expect, it } from 'bun:test';
 
 describe('My Test Suite', () => {
   it('should do something', () => {
-    expect(1 + 1).toBe(2)
-  })
-})
+    expect(1 + 1).toBe(2);
+  });
+});
